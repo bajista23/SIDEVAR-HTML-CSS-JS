dae2e9d1e17d3335f618c33d38a415ebc46ee3bf
bajista23 <157330579+bajista23@users.noreply.github.com>
2024-07-09 16:42:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const sidebar = document.querySelector('.sidebar')
+const toggleBtn = document.querySelector('.toggle-btn');
+
+toggleBtn.addEventListener('click', ()=>{
+
+    sidebar.classList.toggle('active');
+});
